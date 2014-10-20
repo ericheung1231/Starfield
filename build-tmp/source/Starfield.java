f7442c9b1418cf2c00f5e5a003aff61e925c88f6
unknown <ercheung3@697-334-Dale.sf.k12.internal>
2014-10-20 12:24:33 -0700

    First Draft

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,6 +1,22 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 Particle[] bob;
 int opac;
-void setup()
+public void setup()
 {
 	opac = 255;
 	size(500,500);
@@ -15,7 +31,7 @@ void setup()
     bob[i] = new OddballParticle();
   	}
 }
-void draw()
+public void draw()
 {
 	fill(0,0,0,opac);
 	rect(0,0,500,500);
@@ -160,4 +176,13 @@ class OddballParticle implements Particle
 		}
 	}
 	//your code here
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
